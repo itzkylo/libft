2fd5b2d45b664a8192e1263b16084aa9314ab2b5
Kyle JOHNSTON <kjohnsto@c5r2s2.wethinkcode.co.za>
2019-05-24 14:20:27 +0200

    size-bounded string copying and concatenation

---
+++ ft_strlcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kjohnsto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/24 14:09:57 by kjohnsto          #+#    #+#             */
+/*   Updated: 2019/05/24 14:19:50 by kjohnsto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	while (dst[i] && i < dstsize)
+		i++;
+	while ((src[j]) && ((i + j + 1) < dstsize))
+	{
+		dst[i + j] = src[j];
+		j++;
+	}
+	if (i != dstsize)
+		dst[i + j] = '\0';
+	return (j + ft_strlen(src));
+}
