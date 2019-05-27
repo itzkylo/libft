944deb1a41762e4e5ff117574b93333a49196b41
Kyle JOHNSTON <kjohnsto@c5r2s2.wethinkcode.co.za>
2019-05-27 13:21:17 +0200

    Applies the function f to each character of the string given as argument to create a “fresh” new string (with malloc(3)) resulting from the successive applications of f.

---
+++ ft_strmap.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kjohnsto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/27 13:14:58 by kjohnsto          #+#    #+#             */
+/*   Updated: 2019/05/27 13:20:43 by kjohnsto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	unsigned int	i;
+	char			*str;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
+	if (str == NULL)
+		return (NULL)
+			while (s[i] != '\0')
+			{
+				str[i] = f(s[i]);
+				i++;
+			}
+	str[i] = '\0';
+	return (str);
+}
