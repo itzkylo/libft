ed30412519f1b11f29dc74e2adbbb0c243dae269
Kyle JOHNSTON <kjohnsto@c5r3s1.wethinkcode.co.za>
2019-06-20 14:41:06 +0200

    author fixed and FINALLY fixed strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: kjohnsto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 12:52:38 by kjohnsto          #+#    #+#             */
-/*   Updated: 2019/06/20 13:22:29 by kjohnsto         ###   ########.fr       */
+/*   Updated: 2019/06/20 14:34:32 by kjohnsto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ static int	ft_cntwrd(char const *s, char c)
 
 	i = 0;
 	cntr = 0;
-	while (s[i])
+	while (s[i] != '\0')
 	{
 		while (s[i] == c)
 			i++;
@@ -35,8 +35,7 @@ static char	*ft_strndup(const char *s, size_t n)
 {
 	char	*str;
 
-	str = (char *)malloc(sizeof(char) * n + 1);
-	if (str == NULL)
+	if (!(str = (char *)ft_memalloc(sizeof(char) * (n + 1))))
 		return (NULL);
 	str = ft_strncpy(str, s, n);
 	str[n] = '\0';
@@ -45,14 +44,14 @@ static char	*ft_strndup(const char *s, size_t n)
 
 char		**ft_strsplit(char const *s, char c)
 {
-	int		i;
-	int		j;
-	int		k;
-	char	**tab;
+	size_t		i;
+	size_t		j;
+	size_t		k;
+	char		**n;
 
 	i = 0;
 	k = 0;
-	if (!(tab = (char **)malloc(sizeof(char *) * (ft_cntwrd(s, c)) + 1)))
+	if (!s || (!(n = (char **)malloc(sizeof(char *) * (ft_cntwrd(s, c)) + 1))))
 		return (NULL);
 	while (s[i])
 	{
@@ -61,12 +60,12 @@ char		**ft_strsplit(char const *s, char c)
 		j = i;
 		while (s[i] && s[i] != c)
 			i++;
-		if (i < j)
+		if (i > j)
 		{
-			tab[k] = ft_strndup(s + j, i - j);
+			n[k] = ft_strndup(s + j, i - j);
 			k++;
 		}
 	}
-	tab[k] = NULL;
-	return (tab);
+	n[k] = NULL;
+	return (n);
 }
